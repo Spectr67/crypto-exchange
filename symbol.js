0811c67bffc7afbfe76752fd2f53213effe71770
Arthur <pennywennyweed@gmail.com>
2026-07-01 22:53:38 +0300

    added symbols

---
+++ symbol.js
@@ -0,0 +1,8 @@
+class Symbol {
+  constructor(traderId, deal, price, volume) {
+    this.id = crypto.randomUUID().split('-')[0]
+    this.ticker = 'ХЛЕБ'
+    this.fullName
+    this.isTradable
+  }
+}
